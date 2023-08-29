910035edfea046d4129d99258067dc06c543e082
jiacai2050 <dev@liujiacai.net>
2023-08-29 09:26:50 +0800

    add box

--- c-call-rust/main.c
+++ c-call-rust/main.c
@@ -1,6 +1,14 @@
 extern void call_from_c();
 
+struct Foo;
+extern void say_foo( struct Foo*);
+extern struct Foo* new_foo();
+extern void free_foo(struct Foo*);
+
 int main(void) {
     call_from_c();
+    struct Foo* foo = new_foo();
+    say_foo(foo);
+    free_foo(foo);
     return 0;
 }
--- c-call-rust/src/lib.rs
+++ c-call-rust/src/lib.rs
@@ -1,4 +1,43 @@
+use std::ffi::{c_char, c_int, CStr, CString};
+
 #[no_mangle]
 pub extern "C" fn call_from_c() {
     println!("Just called a Rust function from C!");
+}
+
+#[repr(C)]
+pub struct Foo {
+    pub a: c_int,
+    pub b: *const c_char,
+}
+
+#[no_mangle]
+pub extern "C" fn new_foo() -> *mut Foo {
+    let c_str = CString::new("hello world").unwrap();
+    let foo = Box::new(Foo {
+        a: 1,
+        b: c_str.into_raw(),
+    });
+
+    Box::into_raw(foo)
+}
+
+#[no_mangle]
+pub extern "C" fn free_foo(foo: *mut Foo) {
+    if !foo.is_null() {
+        unsafe {
+            let _ = Box::from_raw(foo);
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn say_foo(foo: *const Foo) {
+    if foo.is_null() {
+        println!("None");
+    } else {
+        println!("a = {}, b = {:?}", unsafe { (*foo).a }, unsafe {
+            CStr::from_ptr((*foo).b)
+        });
+    }
 }
